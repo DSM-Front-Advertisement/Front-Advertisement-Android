f4f9cee84d85604e779de1b62028c52cd274cc01
yujaemin <youjmen15@gmail.com>
2020-12-03 10:56:45 +0900

    [ADD] slicing logic test

--- app/src/test/java/com/frontad/frontadvertisement/ExampleUnitTest.java
+++ app/src/test/java/com/frontad/frontadvertisement/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.frontad.frontadvertisement;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -14,4 +17,14 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void regex_slicing_isCorrect(){
+        String link = "https://www.youtube.com/watch?v=uv09yQOTEJ4&ab_channel=%EB%83%A5%EC%8B%A0TV";
+        List<String> a = Arrays.asList(link.split("v="));
+        assertEquals("uv09yQOTEJ4",a.get(1).split("&")[0]);
+    }
+
+//https://www.youtube.com/watch?v=uv09yQOTEJ4&ab_channel=%EB%83%A5%EC%8B%A0TV
+
 }
