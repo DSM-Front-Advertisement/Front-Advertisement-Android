23a1a731e1a2508e38ac61f56ef3ae6b605f0e47
yujaemin <youjmen15@gmail.com>
2020-12-03 11:25:29 +0900

    [ADD] 스플래시 화면

---
+++ app/src/main/java/com/frontad/frontadvertisement/view/SplashFragment.java
@@ -0,0 +1,39 @@
+package com.frontad.frontadvertisement.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.frontad.frontadvertisement.R;
+
+
+public class SplashFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        new Handler(Looper.getMainLooper()).postDelayed(() -> Navigation.findNavController(requireActivity(),R.id.main_container)
+                .navigate(R.id.action_splashFragment_to_mainFragment),2000);
+    }
+}
